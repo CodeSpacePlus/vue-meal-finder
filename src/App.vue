82ed0c93a0e11e29d97f337fac909dce4dd7f305
CodeSpacePlus <bryanc0791@gmail.com>
2020-08-10 21:18:47 -0700

    Backup commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Meal Finder</h1>
+    <Search @search-meal="postResults" @random-meal="postRandom" />
+    <ResultsHeading v-if="meals.length" />
+    <Results :meals="meals" v-if="meals.length" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search';
+import ResultsHeading from './components/Results-Heading';
+import Results from './components/Results';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Search,
+    ResultsHeading,
+    Results,
+  },
+  data() {
+    return {
+      meals: [],
+    };
+  },
+  methods: {
+    postResults(meals) {
+      this.meals = meals;
+    },
+    postRandom(meal) {
+      this.meals = [meal];
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #2d2013;
+  color: #fff;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin: 0;
+}
+
+.container {
+  margin: auto;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
